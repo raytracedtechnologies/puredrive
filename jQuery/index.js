a24291fabd74bdf83eb20e910e627b4260784755
raytracedtechnologies <ekuloey@gmail.com>
2021-12-09 09:03:29 +0800

    Update index.js

--- jQuery/index.js
+++ jQuery/index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     if (navigator.platform = "MacIntel") {
         $('header').append('<h1>Download Pure Drive for Mac</h1>');
     }
-    if (navigator.platform = "Android") {
+    else if (navigator.platform = "Android") {
         $('header').append('<h1>Download Pure Drive for Android</h1>');
     }
 });
