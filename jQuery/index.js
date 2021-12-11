837b35085c40d28b29dc948137a9ef26b54818c3
raytracedtechnologies <ekuloey@gmail.com>
2021-12-11 12:56:42 +0800

    123456789

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     var domArray = ['<header></header>'];
-    $('body').append(domArray);
+    $('main').append(domArray);
     $('body').append("Platform: " + navigator.platform);
     if (navigator.platform = "MacIntel") {
-        $('header').append('<h1>Download Pure Drive for Mac</h1>');
+        $('header').append('<h1>Download Pure Drive</h1>');
     }
     else if ((navigator.platform = "Android") || (navigator.platform = "Linux armv7l") || (navigator.platform = "Linux aarch64")) {
-        $('header').append('<h1>Download Pure Drive for Android</h1>');
+        $('header').append('<h1>Download Pure Drive</h1>');
     }
 });
